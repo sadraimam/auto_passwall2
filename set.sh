2bee4a6b9022e33af57680cc520d7a602bfe3143
Insᴓ. <sadra.imam@gmail.com>
2026-04-12 20:14:44 +0330

    Update set.sh

--- set.sh
+++ set.sh
@@ -161,10 +161,10 @@ ARCH=$(opkg print-architecture | grep -E 'aarch64|arm|mips|x86' | awk '{print $2
 opkg remove dnsmasq
 install_tmp dnsmasq-full
 install_tmp luci-app-passwall2 "https://github.com/Openwrt-Passwall/openwrt-passwall2/releases/download/26.4.10-1/luci-app-passwall2_26.4.10-r1_all.ipk"
-install_tmp ipset
-install_tmp kmod-tun
-install_tmp kmod-nft-tproxy
-install_tmp kmod-nft-socket
+#install_tmp ipset
+#install_tmp kmod-tun
+#install_tmp kmod-nft-tproxy
+#install_tmp kmod-nft-socket
 #install_tmp kmod-netlink-diag
 #install_tmp wget-ssl
 #install_tmp unzip
@@ -173,7 +173,7 @@ install_tmp kmod-nft-socket
 #install_tmp kernel
 install_tmp sing-box "https://github.com/SagerNet/sing-box/releases/download/v1.11.15/sing-box_1.11.15_openwrt_${ARCH}.ipk"
 install_tmp hysteria
-opkg --force-overwrite upgrade luci-app-passwall2
+#opkg --force-overwrite upgrade luci-app-passwall2
 #opkg --force-overwrite upgrade sing-box
 
 # Function to verify installation
